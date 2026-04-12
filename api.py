c2da143308d426fe1692b2b4c0c4a4eacd3c7241
Winston Mwindaare <wmwindaare28@wooster.edu>
2026-04-11 21:37:29 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,24 +1,34 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import psycopg2
 from datetime import datetime
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 try:
     conn = psycopg2.connect(
         dbname="aimtrainer",
-        user="postgres",
-        password="password",
+        user="username",
+        password="",
         host="localhost",
         port="5432"
     )
     cursor = conn.cursor()
-    
+    print("Database connected successfully")
+
 except Exception as e:
     conn = None
     cursor = None
-    print("Database not connected:",e)
+    print("Database not connected:", e)
+
 
 class Score(BaseModel):
     username: str
@@ -27,3 +37,6 @@ class Score(BaseModel):
     accuracy: float
     time: float
     mode: str
+
+
+MODE_MAP = {"T": "Timed", "L": "Lives", "Timed": "Timed", "Lives": "Lives"}
